c3f4f5a2703b89760e9d3acbe56b69c21cb155ce
fizhes <80635091+fizhes@users.noreply.github.com>
2022-10-01 09:33:22 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,185 @@
+
+
+const us = {
+  // us: 'https://lh3.googleusercontent.com/_7dwpiw5wxUrq5J_u6KUBz8ZsdUmZcC0jyfDhvO-__VrGfz7C_0kQ06m_LfcEa6Sw-v28sOU4aq2LKC7MpgjJCFJQbre26C_25QswnnfcMxIVaukq5ZYNNyrN6xEIvE-XGTBYAFtFBjHR9tEUiyS_0kJ1r_CP_82Ckz6Z0eDObxYto6paxwjJb7LOasZxbkq1sMvsohB7gO5TKdvPomCwRVuu5EkBGfysrFawcEA_7aQP_jSH-SjNsCg5HyAVN0vjFjz37bPfNq8tmRfSuRVFjERs5nKmctz585pAelj-JI4ObgSg3dcSSebMe6hNUka1i1gS2WIku8ChfTqhjBM58puT5jkrpdg5ad9FsatFh0oPYsK0E4a-cottwC-xfjGirSrixw9mt2ByWwTe3BQJhbHQwkl59kvlGMmmKIlHYT7DxPjOCD1c1XuMzQclJ6iM44ACXzejBWSvomVURYITEMuNHYDHwx05BhF4OG3902cwcduo532jhk-KUOObtaF5BraZCc0ELh3NBSGywYw5tCbVyaOxMVVFYcQUDUgXi9nVIoBXbz7AxDLN-B-MXckOaCelUGgzPZDoLcMnEGSXgBRDOflfFNGEFRNOh6Tmk952s4pcYogKByKWPJQ4WyWdu4A7ps09NpOEcu7MADZUuPaunkc81ujtI4xl5Al8PoH18fBJ0L6HWYmqBOhhuI0T0dmuknIrSlHiGehphsJlSnb8B7tWdORiGI-VqYqGLzLc49Cq1MzoNUHa1uMsrZtl8D91Kr702TFiwuIHNXkcmKK_gtTF2Z_DJ7CQHT_SLKO-farHaGRKA-exW_uaoJoR-_RbVEQSv5tP_2JHybDyJtK83KnHbTzjwlGnYGGrd3TJgZJFpD6KPVsexe8f9-vvwtEgjsY5wCGB79F7leVQo-nBuTNthcH9hNDk52AlyW4zH6f_vVaAj79pNjqvXDJzjhR40hH0wAh1PlcfVzr1olfEB0RtNwWk5Qh_pwuvpL5xplqhA=w1919-h960-no?authuser=0',
+  us: 'images/us.png',
+};
+const britain = {
+  // uk: 'https://lh3.googleusercontent.com/ckyXcDobsa3qPmfP0MlEd-loyB0TNJjrt3WD-J79oN2R-mPnE_TKuEXVtd6fFeKKycZ_BWzv6c8phay8_iGd4-YFxaJfXLBXSjYYgfqJxzCut0C-MGo1S7POaZ2-HAwK-oq8mrFSVAPtIsS1xQ5xsPU9gqs_Av68hRmwdOsRsjuDgZjy4ThrY1mQ5aOxBXkRan4hVv2EQRjtd1gk4-eCmi82dXHVDT0llB4yRB8A9PcTlV-sHelB4A6zCIlzGrz_cQar49YTREjOg0BiXJzH9_qiq6TTsv9NhvzS4MqWDTpVU6AjhhykvVVv3orV6VJRwazfBGyRyBu6lIixgKoJXOSEnUOz-p3RrcnzqEs6RZlESGFHjadlKSLaP5i5cyGcHuUku06_goQVyTQXRpsiOJAGQ73z2hu0e8Sx1rVqmvt8gkOirVJQPZb-zPxiDLF91rg9YLLe6nFqBo1mn8yXrEGMwPURYd3FJSmskD0muaoVm_LacdwFhHUg1dF1YN9DYS0fPx_mQMZWPGFbkWjZS6qyQccMISW-LYPpPTUt1wTTvCmTfCiNLelqdYQCm8l8gIaP6UN8_RwPGBx33akqRTP7sTYEDjkVjF4NP-I6jUM8de2tQ9KZnn6Inh5lX5krMYk1mu8ov5ooNhaYaTNLcnmnC1PgsJBVwtlpSybHhfuFv5sfPUZxdZ1TFHquWi_tOUSJMNvbgYMd2IgU3TQkynSQQ2w-r56pdyJYP1jbAM88_YjMzPR21UDP7d2ioQYQploCO44DfCaikZrmhjXiDoHeWyILxquRPe3xoTS3MuCHh5rp9859kpo22GueLlXKJgq5OorFK0ECJPUb_KjeIRhb41hn4FfKDOjThLOgrU0dpZClFghv7AqrNmO5mH4c-MlFyo_EkzWmsv3Mm64XeGkw1fRK-ti-4LaIp5el8V4Eo3JI0TYkvkQM2t-1wdo5LIndSTr985cC_fzXkcKQUiLHk1yWZ2nYv_d4GGweUr9AQlr4kA=w1919-h960-no?authuser=0',
+  uk: 'images/uk-rn.png',
+};
+const france = {
+  // rep: 'https://lh3.googleusercontent.com/4cFu-MhVzqjmYSM8pr6Yzauu7tc4OttzX1TpOLv4XGWoEeUOVgRPE-sMxd1NgA_dv_qJoCVGtT-mn4cf9hH-5N3xGO52OqHR1xaXoBmOONHpMMLElubr9NzGaqDByr1y11IhKscigJPMJn8kaWeI4gDGab8GVH5xn-SB3TolTeB2SFOMNbOhnb61jnI9kXzJcC4MkYu3Hv6xJlaY7_fqtBdQA6a_bAbdT8wkhQYk3GCmMATTjhWdqFF4fD4nGGyPV_Ko1QCkjhZfjXre_cQ08O0lZXo9b5aXklAwhA3GAPQ4xqTW9oErTk1-NrZ7IuTDCqQf_3QrT0P3YUCoYGjylR1LGB7PGGeEag_maKUQt0k0g3FRyu7XshNOy4O3iYxsQJIJfthRra0aJ9UKeNxJ18e1CNQ32359OqUZWkmV8eiUTTM9fWa4jvDFQTsVjxPO69RjAqqxSoooNVY1Sd47QIJ_Qs0D2NI90zEcVCDt4NonLQXmuACHyFp8-64dKxq86tGgslcCPKodTBxfNa1NYVpBEFwvUQxwJzEWUUlry-WeXPpkl3tCUqExbgcLXjD1VnKbUGTcAUX__gXG8ILOag9Pma6a7D2x61JzGvkQD-nt46venECvMze8y3iFJXmOGMZi3eMSNv2zC11hBI1HCV2kV6MuPSffiCu6KB60MIZbtGDFfM5p-SAHVOdndyk09of_I9g5aKvoaVBbAC_YzlMCSqm6Cjo6FVckd49lzSnP_1Z2LkLVs75on70N_gobDsSu8i98OwGhYTJedZuqdZ5eKpDf3RD6vCn-vWGMj7s9F_daj2Zm8AOMx4RdjKF86JaSJ3g=w1919-h960-no?authuser=0',
+  rep: 'images/france.png'
+};
+const netherlands = {
+  // kd: 'https://lh3.googleusercontent.com/6hmxO3mYO-4VWNa0e1jhpssI2xeMnfWwn_IVPxPYCCht2jWr8Wj7TF0ZMD1kOKjXSg_Kk4CdcFx6tp4HMmgKiHpzWJF2rsSDFwYTzFSd6hq32yilwF6dT8p-diiCVDQkLONZ-nE8JS5QT9tCc8839dxjUP2JsPyl5X8by3Wc670QyWtr3a5mfaVHGXe82VBpap5tI-Od7JA3oFpscjTKX2xYU6D0g9m-JW3nfa3MvqDccNeeS5FhcEffPk-q8BJ_gdHNibBEuICrUnWCIYDPzUCmrOCchCLbqCmKA0F7lGVUtywZrTlKJSz0ZB-x-laCwBWPRTTQSfl-h5_bS-Io-IoFSa5NnMQ08fh2_reKe1BB_KOVJ3oZqDwby-RPA2JmYnT_ajEII02J-NlDGq8VxfthpU25pcjpCsalyFzGIxXYZjiIWcMjU3ytVWNawHvS0m4IAGPHDUGFzOao-4qv0R44RxUwtgsGcuIA4nzKiDqnHpmSeqqhFUeF3tZGhPVI0PveEWwfKKVk02Dg66H8ikEknCldRslG56EpgWxeEOfA9Z6FiQkEKjqoTYDBE7UE2JaUMTCdM6Ca855iWppFpk0m8S8T_dcw_-WPghtYGg2TtoSU6esmK_n53eaSbMct-PABdx7HsNUNdc3LwvzFJ3ureAKyBsLsZAbglEhWgFQW1iB9mjnn-nzxq1veZakbURmlLp6K1hbZvgqPdZ0JMwlEv9Qlb_E_aDddTZkAulQYVigP_eIMealqjpFb38cmEvB4q1s5hcW9giJMrlDOlRXd4fgxkvBZSHqKkHHCG4C0oQB8gvkiCHZxZZkhPX47o3Y0o1t1ila_b890NrlKwNg_5hKoHYNdfWo5v0XSgrsPCKK0vr0gPWf9S43aMQJ6PwsD2ghbu3wmjBaTcrl7gfRN8BJdnILXcwqYAwyVEnDUrbm7iJrGBP5-4A1njv4xHztgV6bZGAGl6p_aeKc7M6hEKLjT4uRa-CBb98GqoOf4tKA4Jg=w1919-h960-no?authuser=0',
+  kd: 'images/netherlands.png',
+};
+const denmark = {
+  // kd: 'https://lh3.googleusercontent.com/yZBB4cung3Npuf_pu1CwjCr5n01jlLc2sQuZg3iocYxqO72N0lZd_P7k8GsKoF5EGtJBgb8Oz-lqxzITKvirquGxTglL8jJroyfjn9CjxX3FGhvWpnwXeXdpopOlPg4csFGPo2CWbw=w2400',
+  kd: 'images/denmark.png',
+};
+
+let img;
+
+let rot;
+
+let up = false, down = false, left = false, right = false, zi = false, zo = false, zu = false, zd = false;
+
+function keyPressed() {
+  switch(key.toLowerCase()) {
+    case 'w':
+      up = true;
+      break;
+    case 'a':
+      left = true;
+      break;
+    case 's':
+      down = true;
+      break;
+    case 'd':
+      right = true;
+      break;
+    case 'arrowup':
+      zi = true;
+      break;
+    case 'arrowdown':
+      zo = true;
+      break;
+    default:
+      break;
+  }
+}
+
+function keyReleased() {
+  switch(key.toLowerCase()) {
+    case 'w':
+      up = false;
+      break;
+    case 'a':
+      left = false;
+      break;
+    case 's':
+      down = false;
+      break;
+    case 'd':
+      right = false;
+      break;
+    case 'arrowup':
+      zi = false;
+      break;
+    case 'arrowdown':
+      zo = false;
+      break;
+    default:
+      break;
+  }
+}
+
+let rad = 200, det = 16;
+
+
+const ldm = document.querySelector('#ldm');
+ldm.checked = true;
+ldm.onchange = function() {
+  if(ldm.checked) det = 16;
+  else det = 128;
+};
+
+const which = document.querySelector('#which');
+const legend = document.querySelector('#key');
+which.oninput = function() {
+  let url;
+  
+  switch(which.value) {
+    case 'us':
+      rot.x = -0.5; rot.y = 5.5;
+      url = us.us;
+      legend.innerHTML = '<span style="color:#B200FF">\u2588</span><span style="color:white"> States & District of Columbia</span><br/><span style="color:#00FF90">\u2588</span><span style="color:white"> Incorporated, Unorganized</span><br/><span style="color:#FF6A00">\u2588</span><span style="color:white"> Unincorporated, Commonwealth</span><br/><span style="color:#00FFFF">\u2588</span><span style="color:white"> Unincorporated, Organized</span><br/><span style="color:#FFD800">\u2588</span><span style="color:white"> Unincorporated, Unorganized</span><br/><span style="color:#FF0000">\u2588</span><span style="color:white"> Claimed but Disputed</span>';
+      break;
+    case 'uk':
+      rot.x = -0.72; rot.y = 9.35;
+      url = britain.uk;
+      legend.innerHTML = '<span style="color:#B200FF">\u2588</span><span style="color:white"> Constituent Countries</span><br/><span style="color:#FFD800">\u2588</span><span style="color:white"> Crown Dependencies</span><br/><span style="color:#00FF90">\u2588</span><span style="color:white"> Overseas Territories</span><br/><span style="color:#FF0000">\u2588</span><span style="color:white"> Nobody Cares About This</span>';
+      break;
+    case 'fr':
+      rot.x = -0.385; rot.y = 10;
+      url = france.rep;
+      legend.innerHTML = '<span style="color:#B200FF">\u2588</span><span style="color:white"> Metropolitan France</span><br/><span style="color:#00FF90">\u2588</span><span style="color:white"> Overseas Regions/Departments</span><br/><span style="color:#00FFFF">\u2588</span><span style="color:white"> Overseas Collectivities</span><br/><span style="color:#FFD800">\u2588</span><span style="color:white"> Sui Generis Collectivity</span><br/><span style="color:#FF0000">\u2588</span><span style="color:white"> Overseas Territory</span><br/><span style="color:#FF6A00">\u2588</span><span style="color:white"> Overseas State Private Property</span>';
+      break;
+    case 'kon':
+      rot.x = -0.385; rot.y = 10;
+      url = netherlands.kd;
+      legend.innerHTML = '<span style="color:#B200FF">\u2588</span><span style="color:white"> Netherlands (European provinces<br/>& Special Municipalities)</span><br/><span style="color:#00FF90">\u2588</span><span style="color:white"> Other Constituent Countries</span><br/><span style="color:#FF0000">\u25CB</span><span style="color:white"> Component of Dutch Caribbean</span><br/><span style="color:#FFD800">\u25CB</span><span style="color:white"> Component of Caribbean Netherlands</span>';
+      break;
+    case 'kod':
+      rot.x = -0.385; rot.y = 10;
+      url = denmark.kd;
+      legend.innerHTML = '<span style="color:#B200FF">\u2588</span><span style="color:white"> Metropolitan Denmark (Denmark Proper)</span><br/><span style="color:#00FF90">\u2588</span><span style="color:white"> Autonomous Constituent Countries</span><br/><span style="color:#FF0000">\u2588</span><span style="color:white"> KoD Claims Some Arctic<br/>Ridges But I Don\'t Care</span>';
+      break;
+  }
+  
+  img = loadImage(url);
+  
+};
+
+
+function mobile() {
+  let uagent = navigator.userAgent || navigator.vendor || window.opera;
+  return(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(uagent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(uagent.substr(0,4))); 
+}
+
+function setup() {
+  const _s = min(800, window.innerWidth, window.innerHeight);
+  createCanvas(_s, _s, WEBGL);
+  img = loadImage(us.us);
+  rot = createVector(-0.6, 5.205, 0);
+  
+  if(mobile()) {
+    let cam = createEasyCam();
+    cam.setPanScale(0);
+    cam.setRotationScale(0.0025);
+  }
+  
+  document.oncontextmenu = () => false;
+  
+  which.oninput();
+  
+}
+
+
+const dr = 0.015;
+const dz = 0.015;
+
+function draw() {
+  background(0);
+  // image(img, -width/2, -height/2, width, height)
+  
+  push();
+
+  if(right) rot.y -= dr;
+  if(left)  rot.y += dr;
+  if(up)    rot.x -= dr;
+  if(down)  rot.x += dr;
+  
+  if(zi) rad *= 1.01;
+  if(zo) rad /= 1.01;
+  
+  
+  rotateX(rot.x);
+  rotateY(rot.y);
+  rotateZ(rot.z);
+  
+  noStroke();
+  fill(255);
+  texture(img);
+  sphere(rad, det, det);
+  
+  pop();
+    
+  
+}
+
+
+
